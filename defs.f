e3635b8d78acbd04bd4565aa032afecea8aa9b25
Matthew Pharr <matthewcpharr@gmail.com>
2025-07-01 15:14:16 +0200

    SPLINES - NEW - import fortran splines, made c api for spline interactions with julia

---
+++ defs.f
@@ -0,0 +1,222 @@
+c-----------------------------------------------------------------------
+c     file defs.f
+c     local defintions for most computers.
+c-----------------------------------------------------------------------
+c-----------------------------------------------------------------------
+c     code organization.
+c-----------------------------------------------------------------------
+c     0. defs_mod.
+c-----------------------------------------------------------------------
+      module defs_mod
+      implicit none
+      
+      integer, parameter ::
+     $     r4=selected_real_kind(6,37),
+     $     r8=selected_real_kind(15,307),
+     $     r16=selected_real_kind(33,4931),
+     $     i4=selected_int_kind(9),
+     $     i8=selected_int_kind(18)
+
+      real(r8) :: pi = 3.14159265358979323846_r8
+      real(r8) :: twopi = 6.28318530717958647692_r8
+      real(r8) :: zero = 0.0_r8
+      real(r8) :: one = 1.0_r8
+      real(r8) :: two = 2.0_r8
+      real(r8) :: half = 0.5_r8
+      integer :: bin_unit = 10
+      integer :: out_unit = 11
+
+      contains
+c-----------------------------------------------------------------------
+c     subprogram 1. timer.
+c     handles machine-dependent timing statistics.
+c-----------------------------------------------------------------------
+c-----------------------------------------------------------------------
+c     delcarations.
+c-----------------------------------------------------------------------
+      SUBROUTINE timer(mode,unit,op_cpuseconds,op_wallseconds)
+      
+      INTEGER, INTENT(IN) :: mode,unit
+      REAL(r4), INTENT(OUT), OPTIONAL :: op_cpuseconds,op_wallseconds
+
+      INTEGER(8), SAVE :: count_rate, wall_start
+      REAL(r4), SAVE :: start
+      REAL(r4) :: seconds
+      INTEGER(8) :: hrs,mins,secs, wall_seconds, count_max
+c-----------------------------------------------------------------------
+c     format statements.
+c-----------------------------------------------------------------------
+ 10   FORMAT(1x,a,1p,e10.3,a)
+c-----------------------------------------------------------------------
+c     computations.
+c-----------------------------------------------------------------------
+      IF(mode == 0)THEN
+         CALL CPU_TIME(start)
+         CALL SYSTEM_CLOCK(wall_start)
+      ELSE
+         ! report cpu time
+         CALL CPU_time(seconds)
+         seconds=seconds-start
+         secs = int(seconds)
+         hrs = secs/(60*60)
+         mins = (secs-hrs*60*60)/60
+         secs = secs-hrs*60*60-mins*60
+         IF(PRESENT(op_cpuseconds))THEN
+            ! simply provide the time to the caller
+            op_cpuseconds = seconds
+         ELSE
+            ! write the time to terminal and file
+            IF(hrs>0)THEN
+               WRITE(*,'(1x,a,i3,a,i2,a,i2,a)') "Total cpu time = ",
+     $            hrs," hours, ",mins," minutes, ",secs," seconds"
+            ELSEIF(mins>0)THEN
+               WRITE(*,'(1x,a,i2,a,i2,a)') "Total cpu time = ",
+     $            mins," minutes, ",secs," seconds"
+            ELSEIF(secs>0)THEN
+               WRITE(*,'(1x,a,i2,a)') "Total cpu time = ",secs,
+     $            " seconds"
+            ENDIF
+            WRITE(unit,10) "Total cpu time = ",seconds," seconds"
+         ENDIF
+         ! report wall time
+         CALL SYSTEM_CLOCK(wall_seconds, count_rate, count_max)
+         seconds=(wall_seconds-wall_start)/REAL(count_rate, 8)
+         secs = int(seconds)
+         hrs = secs/(60*60)
+         mins = (secs-hrs*60*60)/60
+         secs = secs-hrs*60*60-mins*60
+         IF(PRESENT(op_wallseconds))THEN
+            op_wallseconds = seconds
+         ELSE
+            IF(hrs>0)THEN
+               WRITE(*,'(1x,a,i3,a,i2,a,i2,a)') "Total wall time = ",
+     $            hrs," hours, ",mins," minutes, ",secs," seconds"
+            ELSEIF(mins>0)THEN
+               WRITE(*,'(1x,a,i2,a,i2,a)') "Total wall time = ",
+     $            mins," minutes, ",secs," seconds"
+            ELSEIF(secs>0)THEN
+               WRITE(*,'(1x,a,i2,a)') "Total wall time = ",secs,
+     $            " seconds"
+            ENDIF
+            WRITE(unit,10) "Total wall time = ",seconds," seconds"
+         ENDIF
+      ENDIF
+c-----------------------------------------------------------------------
+c     terminate.
+c-----------------------------------------------------------------------
+      RETURN
+      END SUBROUTINE timer
+c-----------------------------------------------------------------------
+c     subprogram 2. bin_open.
+c     opens a binary input or output file.
+c-----------------------------------------------------------------------
+c-----------------------------------------------------------------------
+c     declarations.
+c-----------------------------------------------------------------------
+      SUBROUTINE bin_open(unit,name,stat,pos,convert_type)
+
+      CHARACTER(*), INTENT(IN) :: name,stat,pos,convert_type
+      INTEGER, INTENT(IN) :: unit
+c-----------------------------------------------------------------------
+c     open file.
+c-----------------------------------------------------------------------
+      SELECT CASE(convert_type)
+      CASE("none")
+         OPEN(UNIT=unit,FILE=name,STATUS=stat,POSITION=pos,
+     $        FORM="UNFORMATTED")
+      CASE("big")
+         OPEN(UNIT=unit,FILE=name,STATUS=stat,POSITION=pos,
+     $        FORM="UNFORMATTED",CONVERT="BIG_ENDIAN")
+      CASE("little")
+         OPEN(UNIT=unit,FILE=name,STATUS=stat,POSITION=pos,
+     $        FORM="UNFORMATTED",CONVERT="LITTLE_ENDIAN")
+      CASE DEFAULT
+         CALL program_stop
+     $        ("Cannot recognize convert_type = "//TRIM(convert_type))
+      END SELECT
+c-----------------------------------------------------------------------
+c     terminate.
+c-----------------------------------------------------------------------
+      RETURN
+      END SUBROUTINE bin_open
+c-----------------------------------------------------------------------
+c     subprogram 3. bin_close.
+c     close a binary input or output file.
+c-----------------------------------------------------------------------
+c-----------------------------------------------------------------------
+c     declarations.
+c-----------------------------------------------------------------------
+      SUBROUTINE bin_close(unit)
+
+      INTEGER, INTENT(IN) :: unit
+c-----------------------------------------------------------------------
+c     work.
+c-----------------------------------------------------------------------
+      CLOSE(UNIT=unit)
+c-----------------------------------------------------------------------
+c     terminate.
+c-----------------------------------------------------------------------
+      RETURN
+      END SUBROUTINE bin_close
+c-----------------------------------------------------------------------
+c     subprogram 4. ascii_open.
+c     opens a ascii input or output file.
+c-----------------------------------------------------------------------
+c-----------------------------------------------------------------------
+c     declarations.
+c-----------------------------------------------------------------------
+      SUBROUTINE ascii_open(unit,name,stat)
+
+      CHARACTER(*), INTENT(IN) :: name,stat
+      INTEGER, INTENT(IN) :: unit
+c-----------------------------------------------------------------------
+c     open file.
+c-----------------------------------------------------------------------
+      OPEN(UNIT=unit,FILE=name,STATUS=stat)
+c-----------------------------------------------------------------------
+c     terminate.
+c-----------------------------------------------------------------------
+      RETURN
+      END SUBROUTINE ascii_open
+c-----------------------------------------------------------------------
+c     subprogram 5. ascii_close.
+c     close a ascii input or output file.
+c-----------------------------------------------------------------------
+c-----------------------------------------------------------------------
+c     declarations.
+c-----------------------------------------------------------------------
+      SUBROUTINE ascii_close(unit)
+
+      INTEGER, INTENT(IN) :: unit
+c-----------------------------------------------------------------------
+c     work.
+c-----------------------------------------------------------------------
+      CLOSE(UNIT=unit)
+c-----------------------------------------------------------------------
+c     terminate.
+c-----------------------------------------------------------------------
+      RETURN
+      END SUBROUTINE ascii_close
+c-----------------------------------------------------------------------
+c     subprogram 6. program_stop.
+c     terminates program with message, calls timer, closes output file.
+c-----------------------------------------------------------------------
+c-----------------------------------------------------------------------
+c     declarations.
+c-----------------------------------------------------------------------
+      SUBROUTINE program_stop(message)
+
+      CHARACTER(*), INTENT(IN) :: message
+c-----------------------------------------------------------------------
+c     write completion message.
+c-----------------------------------------------------------------------
+      CALL timer(1,out_unit)
+      CALL ascii_close(out_unit)
+      WRITE(*,'(1x,2a)') 'PROGRAM STOP => ', TRIM(message)
+c-----------------------------------------------------------------------
+c     write completion message.
+c-----------------------------------------------------------------------
+      STOP
+      END SUBROUTINE program_stop
+
+      end module defs_mod
---
+++ spline_c_api.f
@@ -0,0 +1,373 @@
+c-----------------------------------------------------------------------
+c     file spline_c_api.f
+c     an interface to the spline libraries for c variables.
+c-----------------------------------------------------------------------
+c-----------------------------------------------------------------------
+c     code organization.
+c-----------------------------------------------------------------------
+c     0. spline_c_api_mod.
+c.    1. spline_c_create
+c     2. spline_c_destroy
+c     3. spline_c_setup
+c     4. spline_c_fit
+c     5. spline_c_eval
+c     6. spline_c_eval_deriv
+c     7. spline_c_eval_deriv2
+c     8. spline_c_eval_deriv3
+c-----------------------------------------------------------------------
+c     subprogram 0. spline_c_api_mod
+c     module declarations.
+c-----------------------------------------------------------------------
+      module spline_c_api_mod
+      use iso_c_binding
+      use spline_mod
+      implicit none
+c-----------------------------------------------------------------------
+c     declarations.
+c-----------------------------------------------------------------------
+      type, bind(C) :: spline_handle
+         type(c_ptr) :: obj
+      end type spline_handle
+
+      logical :: debug = .true.
+
+      contains
+c-----------------------------------------------------------------------
+c     subprogram 1. spline_c_create
+c     allocates a spline object
+c-----------------------------------------------------------------------
+      subroutine spline_c_create(mx, nqty, handle) bind(C)
+c-----------------------------------------------------------------------
+c     declarations.
+c-----------------------------------------------------------------------
+      integer(c_int), value :: mx, nqty
+      type(spline_handle), intent(out) :: handle
+      type(spline_type), pointer :: spl
+
+      allocate(spl)
+      call spline_alloc(spl, mx, nqty)
+      handle%obj = c_loc(spl)
+c-----------------------------------------------------------------------
+c     terminate.
+c-----------------------------------------------------------------------
+      return
+      end subroutine spline_c_create
+c-----------------------------------------------------------------------
+c     subprogram 2. spline_c_destroy
+c     deallocates a spline object
+c-----------------------------------------------------------------------
+      subroutine spline_c_destroy(handle) bind(C)
+c-----------------------------------------------------------------------
+c     declarations.
+c-----------------------------------------------------------------------
+      type(spline_handle), value :: handle
+      type(spline_type), pointer :: spl
+      
+      call c_f_pointer(handle%obj, spl)
+      if (associated(spl)) then
+         call spline_dealloc(spl)
+         deallocate(spl)
+      else
+         print *, "spline_c_destroy: handle is not associated "
+     $       // "with a valid spline object." 
+      end if
+c-----------------------------------------------------------------------
+c     terminate.
+c-----------------------------------------------------------------------
+      return
+      end subroutine spline_c_destroy
+c-----------------------------------------------------------------------
+c     subprogram 3. spline_c_setup
+c     sets up the spline object with data.
+c-----------------------------------------------------------------------
+      subroutine spline_c_setup(handle, xs, fs) bind(C)
+c-----------------------------------------------------------------------
+c     declarations.
+c-----------------------------------------------------------------------
+      type(spline_handle), value :: handle
+      type(c_ptr), value :: xs, fs
+
+      type(spline_type), pointer :: spl
+      real(c_double), pointer :: x(:), f(:, :)
+      integer(i8) :: mx, nqty
+      integer(i4) :: i
+c-----------------------------------------------------------------------
+c     work.
+c-----------------------------------------------------------------------
+      call c_f_pointer(handle%obj, spl)
+      if (.not. associated(spl)) then
+         print *, "spline_c_setup: handle is not associated "
+     $       // "with a valid spline object."
+         return
+      end if
+
+      mx = spl%mx
+      nqty = spl%nqty
+
+      call c_f_pointer(xs, x, [mx+1])
+      call c_f_pointer(fs, f, [mx+1, nqty])
+
+      ! do i = 0, mx
+      !    spl%xs(i) = x(i+1)  ! Fortran is 1-based, C is 0-based
+      !    spl%fs(i, 1:nqty) = f(i+1, 1:nqty)
+      ! end do
+
+      spl%xs = x
+      spl%fs = f
+
+      if (debug) then
+         print *, "spline_c_setup: setting up spline with "
+     $       // "mx = ", mx, " and nqty = ", nqty
+         print *, "xs = ", x(1:mx+1)
+         print *, "fs = "
+     $       // "(", nqty, " quantities):"
+         do i = 1, nqty
+            print *, "  fs(:,", i, ") = ", f(:, i)
+         end do
+      end if
+c------------------------------------------------------------------------
+c     terminate.
+c------------------------------------------------------------------------
+      return 
+      end subroutine spline_c_setup
+c-----------------------------------------------------------------------
+c     subprogram 4. spline_c_fit
+c     fits the spline to the data.
+c-----------------------------------------------------------------------
+      subroutine spline_c_fit(handle, endmode) bind(C)
+c-----------------------------------------------------------------------
+c     declarations.
+c-----------------------------------------------------------------------
+      type(spline_handle), value :: handle
+      integer(c_int), value :: endmode
+      type(spline_type), pointer :: spl
+
+      character(10) :: endmode_str
+c-----------------------------------------------------------------------
+c     work.
+c-----------------------------------------------------------------------
+      call c_f_pointer(handle%obj, spl)
+      if (.not. associated(spl)) then
+            print *, "spline_c_fit: handle is not associated "
+     $       // "with a valid spline object."
+            return
+      end if
+
+      print *, "test"
+
+      select case(endmode)
+      case(1)
+         endmode_str = "natural"
+      case(2)
+         endmode_str = "periodic"
+      case(3)
+         endmode_str = "extrap"
+      case(4)
+         endmode_str = "not-a-knot"
+      end select
+      if (debug) then
+         print *, "spline_c_fit: fitting spline with endmode = "
+     $       // TRIM(endmode_str)
+      end if
+      call spline_fit(spl, TRIM(endmode_str))
+c-----------------------------------------------------------------------
+c     terminate.
+c-----------------------------------------------------------------------
+      return
+      end subroutine spline_c_fit
+c-----------------------------------------------------------------------
+c     subprogram 5. spline_c_eval
+c     evaluates the spline at a given point.
+c-----------------------------------------------------------------------
+      subroutine spline_c_eval(handle, x, f, ix_op) bind(C)
+c-----------------------------------------------------------------------
+c     declarations.
+c-----------------------------------------------------------------------
+      type(spline_handle), value :: handle
+      real(c_double), value :: x
+      type(c_ptr), value :: f
+      integer(c_int), intent(in), optional :: ix_op
+
+      integer(i4) :: ix ! index of x position in the spline
+      real(c_double), pointer :: fi(:)
+      type(spline_type), pointer :: spl
+c-----------------------------------------------------------------------
+c     work.
+c-----------------------------------------------------------------------
+      call c_f_pointer(handle%obj, spl)
+      if (.not. associated(spl)) then
+         print *, "spline_c_eval: handle is not associated "
+     $       // "with a valid spline object."
+         return
+      end if
+
+      call c_f_pointer(f, fi, [spl%nqty])
+
+      if (present(ix_op)) then
+         ix = ix_op
+      else
+         ix = 0
+      end if
+
+      call spline_eval_external(spl, x, ix, fi)
+c-----------------------------------------------------------------------
+c     copy results back to the C pointer.
+c-----------------------------------------------------------------------
+      call c_f_pointer(f, fi, [spl%nqty])
+c-----------------------------------------------------------------------
+c     terminate.
+c-----------------------------------------------------------------------
+      return
+      end subroutine spline_c_eval
+c-----------------------------------------------------------------------
+c     subprogram 6. spline_c_eval_deriv
+c     evaluates the spline and its first derivative at a given point.
+c-----------------------------------------------------------------------
+      subroutine spline_c_eval_deriv(handle, x, f, f1, ix_op) bind(C)
+c-----------------------------------------------------------------------
+c     declarations.
+c-----------------------------------------------------------------------
+      type(spline_handle), value :: handle
+      real(c_double), value :: x
+      type(c_ptr), value :: f, f1
+      integer(c_int), intent(in), optional :: ix_op
+
+      integer(i4) :: ix ! index of x position in the spline
+      real(c_double), pointer :: fi(:), f1i(:)
+      type(spline_type), pointer :: spl
+c-----------------------------------------------------------------------
+c     work.
+c-----------------------------------------------------------------------
+      call c_f_pointer(handle%obj, spl)
+      if (.not. associated(spl)) then
+         print *, "spline_c_eval: handle is not associated "
+     $       // "with a valid spline object."
+         return
+      end if
+
+      call c_f_pointer(f, fi, [spl%nqty])
+      call c_f_pointer(f1, f1i, [spl%nqty])
+
+      if (present(ix_op)) then
+         ix = ix_op
+      else
+         ix = 0
+      end if
+
+      call spline_eval_external(spl, x, ix, fi, f1i)
+c-----------------------------------------------------------------------
+c     copy results back to the C pointer.
+c-----------------------------------------------------------------------
+      call c_f_pointer(f, fi, [spl%nqty])
+      call c_f_pointer(f1, f1i, [spl%nqty])
+
+c-----------------------------------------------------------------------
+c     terminate.
+c-----------------------------------------------------------------------
+      return
+      end subroutine spline_c_eval_deriv
+c-----------------------------------------------------------------------
+c     subprogram 7. spline_c_eval_deriv_2
+c     evaluates the spline and its first and\
+c     second derivatives at a given point.
+c-----------------------------------------------------------------------
+      subroutine spline_c_eval_deriv_2(handle, x, f, f1,
+     $     f2, ix_op) bind(C)
+c-----------------------------------------------------------------------
+c     declarations.
+c-----------------------------------------------------------------------
+      type(spline_handle), value :: handle
+      real(c_double), value :: x
+      type(c_ptr), value :: f, f1, f2
+      integer(c_int), intent(in), optional :: ix_op
+
+      integer(i4) :: ix ! index of x position in the spline
+      real(c_double), pointer :: fi(:), f1i(:), f2i(:)
+      type(spline_type), pointer :: spl
+c-----------------------------------------------------------------------
+c     work.
+c-----------------------------------------------------------------------
+      call c_f_pointer(handle%obj, spl)
+      if (.not. associated(spl)) then
+         print *, "spline_c_eval: handle is not associated "
+     $       // "with a valid spline object."
+         return
+      end if
+
+      call c_f_pointer(f, fi, [spl%nqty])
+      call c_f_pointer(f1, f1i, [spl%nqty])
+      call c_f_pointer(f2, f2i, [spl%nqty])
+
+      if (present(ix_op)) then
+         ix = ix_op
+      else
+         ix = 0
+      end if
+
+      call spline_eval_external(spl, x, ix, fi, f1i, f2i)
+c-----------------------------------------------------------------------
+c     copy results back to the C pointer.
+c-----------------------------------------------------------------------
+      call c_f_pointer(f, fi, [spl%nqty])
+      call c_f_pointer(f1, f1i, [spl%nqty])
+      call c_f_pointer(f2, f2i, [spl%nqty])
+
+c-----------------------------------------------------------------------
+c     terminate.
+c-----------------------------------------------------------------------
+      return
+      end subroutine spline_c_eval_deriv_2
+c-----------------------------------------------------------------------
+c     subprogram 8. spline_c_eval_deriv_3
+c     evaluates the spline and its first derivative at a given point.
+c-----------------------------------------------------------------------
+      subroutine spline_c_eval_deriv_3(handle, x, f, f1,
+     $    f2, f3, ix_op) bind(C)
+c-----------------------------------------------------------------------
+c     declarations.
+c-----------------------------------------------------------------------
+      type(spline_handle), value :: handle
+      real(c_double), value :: x
+      type(c_ptr), value :: f, f1, f2, f3
+      integer(c_int), intent(in), optional :: ix_op
+
+      integer(i4) :: ix ! index of x position in the spline
+      real(c_double), pointer :: fi(:), f1i(:), f2i(:), f3i(:)
+      type(spline_type), pointer :: spl
+c-----------------------------------------------------------------------
+c     work.
+c-----------------------------------------------------------------------
+      call c_f_pointer(handle%obj, spl)
+      if (.not. associated(spl)) then
+         print *, "spline_c_eval: handle is not associated "
+     $       // "with a valid spline object."
+         return
+      end if
+
+      call c_f_pointer(f, fi, [spl%nqty])
+      call c_f_pointer(f1, f1i, [spl%nqty])
+      call c_f_pointer(f2, f2i, [spl%nqty])
+      call c_f_pointer(f3, f3i, [spl%nqty])
+
+      if (present(ix_op)) then
+         ix = ix_op
+      else
+         ix = 0
+      end if
+
+      call spline_eval_external(spl, x, ix, fi, f1i, f2i, f3i)
+c-----------------------------------------------------------------------
+c     copy results back to the C pointer.
+c-----------------------------------------------------------------------
+      call c_f_pointer(f, fi, [spl%nqty])
+      call c_f_pointer(f1, f1i, [spl%nqty])
+      call c_f_pointer(f2, f2i, [spl%nqty])
+      call c_f_pointer(f3, f3i, [spl%nqty])
+
+c-----------------------------------------------------------------------
+c     terminate.
+c-----------------------------------------------------------------------
+      return
+      end subroutine spline_c_eval_deriv_3
+c-----------------------------------------------------------------------
+      end module spline_c_api_mod
