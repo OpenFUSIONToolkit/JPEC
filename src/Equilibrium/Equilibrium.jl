e69f97b806fabe9c26f3e62b9e5fa78fa78a5882
Jake Halpern <jhalpern@purdue.edu>
2025-08-28 11:54:44 -0400

    EQUIL - BUGFIX - typo

--- src/Equilibrium/Equilibrium.jl
+++ src/Equilibrium/Equilibrium.jl
@@ -57,7 +57,7 @@ function setup_equilibrium(eq_config::EquilConfig, additional_input=nothing)
         eq_input = lar_run(eq_config, additional_input)
     elseif eq_type == "sol"
 
-        if additonal_input === nothing
+        if additional_input === nothing
             additional_input = SolevevConfig(eq_config.control.eq_filename) 
         end
 
